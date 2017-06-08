2b6893c34a56a3fd76ad6353aa9b1d6439ba8c07
Lucas Marques <cmarques.lucas@gmail.com>
2017-06-08 12:59:51 -0300

    sorting

---
+++ src/main/java/com/lucasmarques/sorting/Chapter5.java
@@ -0,0 +1,37 @@
+package com.lucasmarques.sorting;
+
+import com.lucasmarques.lambda.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by lucascmarques on 08/06/17.
+ */
+public class Chapter5 {
+
+    public static void main(String[] args) {
+
+        User user1 = new User("user1", 100, null);
+        User user2 = new User("user2", 120, null);
+        User user3 = new User("user3", 80, null);
+
+        List<User> listUser = new ArrayList<User>();
+        listUser.add(user3);
+        listUser.add(user1);
+        listUser.add(user2);
+
+        listUser.sort((u1, u2) -> u1.getName().compareTo(u2.getName()));
+        System.out.println(listUser);
+
+        listUser.sort(Comparator.comparing(u -> u.getPoints()));
+        System.out.println(listUser);
+
+        List<String> palavras = Arrays.asList("Casa do Código", "Alura", "Caelum");
+        palavras.sort(Comparator.naturalOrder());
+
+    }
+
+}
