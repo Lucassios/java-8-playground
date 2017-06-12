438039b84e867a6b4bc54b2fd3408127be3913af
Lucas Marques <cmarques.lucas@gmail.com>
2017-06-12 20:07:41 -0300

    method reference

--- src/main/java/com/lucasmarques/lambda/User.java
+++ src/main/java/com/lucasmarques/lambda/User.java
@@ -2,16 +2,22 @@ package com.lucasmarques.lambda;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by lucascmarques on 04/06/17.
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private String name;
     private int points;
     private String moderator;
+
+    public User(String name) {
+        this.name = name;
+    }
 
 }
---
+++ src/main/java/com/lucasmarques/methodreference/Chapter6.java
@@ -0,0 +1,52 @@
+package com.lucasmarques.methodreference;
+
+import com.lucasmarques.lambda.User;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * Created by lucascmarques on 12/06/17.
+ */
+public class Chapter6 {
+
+    public static void main(String[] args) {
+
+        User user1 = new User("user1", 100, null);
+        User user2 = new User("user2", 120, null);
+        User user3 = new User("user3", 80, null);
+
+        List<User> listUser = new ArrayList<User>();
+        listUser.add(user3);
+        listUser.add(user1);
+        listUser.add(user2);
+
+        Function<User, String> functionGetName = User::getName;
+        listUser.sort(Comparator.comparing(functionGetName));
+        listUser.sort(Comparator.comparing(User::getName));
+
+        Comparator.comparingInt(User::getPoints).thenComparing(User::getName);
+
+        User lucas = new User("lucas", 100, null);
+        Runnable getPointsRunnable = lucas::getPoints;
+        getPointsRunnable.run();
+
+        Consumer<User> consumerUser = User::getModerator;
+        consumerUser.accept(lucas);
+
+        listUser.forEach(System.out::println);
+
+        Supplier<User> userFactory = User::new;
+        User user4 = userFactory.get();
+
+        Function<String, User> userContructorName = User::new;
+        User user5 = userContructorName.apply("user5");
+
+
+    }
+
+}
